bde083989f703a13e738a202fde65827f0c6aea6
Morgan234-ui <edachemorgan10@gmail.com>
2025-08-14 14:53:45 +0100

    my project changes

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,9 +1,11 @@
+import HeaderRest from '@/components/Layout/HeaderRest'
 import Signup from '@/components/Pages/Signup/Signupper'
 import React from 'react'
 
 const page = () => {
   return (
        <React.Fragment>
+        <HeaderRest />
         <main className='signup'>
             <Signup />
         </main>
